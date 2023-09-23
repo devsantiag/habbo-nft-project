38daee1dad8ee467e2e7ec7230b946aa9007de52
santiago <contatofasantiago@hotmail.com>
2023-09-23 11:22:07 -0300

    added new images in the spin-preview

--- scripts/pageScript.js
+++ scripts/pageScript.js
@@ -1,4 +1,3 @@
-
 const buttonSpinGaleriaPhotos = document.getElementById('button_spin_roulette')
 let imageElement = document.getElementById('src_script_image');
 const buttonShowPreviewGaleria = document.getElementById('button_preview_content_element')
@@ -12,6 +11,10 @@ const openSeaStorageImageHabboNft = [
     'https://i.seadn.io/gcs/files/390b3589c9bca32dfef09ffe5b23d429.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/3f35bc4e5171750c4a8764abeaa99b39.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/7dfffda22fbe2c3791c2f83d122c4e98.png?auto=format&dpr=1&w=384', 
+    'https://i.seadn.io/gcs/files/7dfffda22fbe2c3791c2f83d122c4e98.png?auto=format&dpr=1&w=384', 
+    'https://i.seadn.io/gcs/files/7dfffda22fbe2c3791c2f83d122c4e98.png?auto=format&dpr=1&w=384', 
+    'https://i.seadn.io/gcs/files/7dfffda22fbe2c3791c2f83d122c4e98.png?auto=format&dpr=1&w=384', 
+    'https://i.seadn.io/gcs/files/7dfffda22fbe2c3791c2f83d122c4e98.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/8ac2b20176465df7d3d19c755d6e957e.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/a5f1daafe3f44556914dd6d332c18ca4.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/88ff6e907caa8352b46eb8808d1f5f7f.png?auto=format&dpr=1&w=384', 
@@ -21,10 +24,13 @@ const openSeaStorageImageHabboNft = [
     'https://i.seadn.io/gcs/files/8598f852eee4e743634cea3add9f46a4.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/1545bc646f802058737bb6443d2a7622.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/8598f852eee4e743634cea3add9f46a4.png?auto=format&dpr=1&w=384', 
-    'https0://i.seadn.io/gcs/files/14b119d17a5182964ea8b97155763ce7.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/14b119d17a5182964ea8b97155763ce7.png?auto=format&dpr=1&w=384', 
     'https://i.seadn.io/gcs/files/14b119d17a5182964ea8b97155763ce7.png?auto=format&dpr=1&w=384', 
-    'https://i.seadn.io/gcs/files/58fbbf4a5eb842b4f6ac1a545b87d738.png?auto=format&dpr=1&w=1000'
+    'https://i.seadn.io/gcs/files/14b119d17a5182964ea8b97155763ce7.png?auto=format&dpr=1&w=384', 
+    'https://i.seadn.io/gcs/files/14b119d17a5182964ea8b97155763ce7.png?auto=format&dpr=1&w=384', 
+    'https://i.seadn.io/gcs/files/14b119d17a5182964ea8b97155763ce7.png?auto=format&dpr=1&w=384', 
+    'https://i.seadn.io/gcs/files/14b119d17a5182964ea8b97155763ce7.png?auto=format&dpr=1&w=384', 
+    'https://i.seadn.io/gcs/files/58fbbf4a5eb842b4f6ac1a545b87d738.png?auto=format&dpr=1&w=1000',
 ];
 
 // initial position of the image that shows to the customer
